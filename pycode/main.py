fd1e1633c9828e8f4ebb3c834eb30e7efaa8d949
navid-kamal <navidkamal568@gmail.com>
2025-03-16 02:11:30 +0600

    feat: use chain to make a function to do tasks in any language

--- pycode/main.py
+++ pycode/main.py
@@ -1,5 +1,7 @@
 import os
 from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import SystemMessagePromptTemplate, HumanMessagePromptTemplate
 from getpass import getpass
 
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY") or getpass(
@@ -9,8 +11,20 @@ os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY") or getpass(
 openai_model = "gpt-4o-mini"
 llm=ChatOpenAI(temperature=0,model=openai_model)
 creative_llm=ChatOpenAI(temperature=0.9,model=openai_model)
+system_prompt = SystemMessagePromptTemplate.from_template(
+    "You are a AI chat assistant that helps generate article titles",
+    
+)
+user_prompt=HumanMessagePromptTemplate.from_template(
+        """ Write a very short {language} function that will {task}""",
+    input_variables=["language","task"]
 
-
-result=creative_llm.invoke("write a very short poem").content
+)
+prompt=ChatPromptTemplate.from_messages(
+[ system_prompt,
+ user_prompt]
+)
+chain= prompt | llm
+result=chain.invoke({"language":"python","task":"print 5 numbers"}).content
 
 print(result)
